d682211035a4ea2bc5bfd3ca835e89e812f544fa
David Li <davli@bess-f4r2s13.clusters.42paris.fr>
2024-05-16 18:11:15 +0200

    s

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,18 +6,31 @@
 /*   By: davli <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 17:14:30 by davli             #+#    #+#             */
-/*   Updated: 2024/05/16 15:25:25 by davli            ###   ########.fr       */
+/*   Updated: 2024/05/16 16:37:02 by davli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-void	*memset(void *s, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t len)
 {
-	size_t	i;
+	unsigned char	*str;
 
-	i = 0;
-	while ()
-	{
-
-	}
-	return (s)
+	str = (unsigned char *)s;
+	while (len)
+		str[--len] = c;
+	return (s);
 }
+/*
+#include <stdio.h>
+int	main()
+{
+	int	i = 0;
+	char	tab[5];
+	ft_memset(tab, 's', 5);
+	while (i < 5)
+	{
+		printf("%c", tab[i]);
+		i++;
+	}
+}
+*/
