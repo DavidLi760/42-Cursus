edb79b93baf8e3af7d58f2ab75359cfa9e5212c8
David <linebulios2003@gmail.com>
2024-05-23 00:48:16 +0200

    Bonus

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*head;
+
+	head = malloc(sizeof(t_list));
+	if (!head)
+		return (NULL);
+	head->content = content;
+	head->next = NULL;
+	head->prev = NULL;
+	return (head);
+}
