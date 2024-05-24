05a7219d12e44515188ddb77634ad8eddd7391f2
David Li <davli@bess-f2r1s3.clusters.42paris.fr>
2024-05-24 13:38:34 +0200

    s

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: davli <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 12:07:31 by davli             #+#    #+#             */
-/*   Updated: 2024/05/24 12:07:32 by davli            ###   ########.fr       */
+/*   Updated: 2024/05/24 13:38:27 by davli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,44 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (size);
 }
+/*
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	t_list	*lst = NULL;
+	t_list	*current = NULL;
+
+	int	i = 1;
+	while (i < argc)
+	{
+		i++;
+		t_list *new_elem = ft_lstnew(argv[i]);
+		if (!new_elem)
+		{
+			while (lst)
+			{
+				current = lst;
+				lst = lst->next;
+				free(current);
+			}
+			return (1);
+		}
+
+		if (lst == NULL)
+		{
+			lst = new_elem;
+			current = lst;
+		}
+		else
+		{
+			current->next = new_elem;
+			current = current->next;
+		}
+	}
+
+	int size = ft_lstsize(lst);
+	printf("Size of the list: %d\n", size);
+	return (0);
+}
+*/
