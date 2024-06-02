62255b7a87fc4afe049dc68a1e45c2b8f9855fba
David <linebulios2003@gmail.com>
2024-06-02 22:59:27 +0200

    s

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -3,85 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: handler <handler@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/18 19:16:30 by handler           #+#    #+#             */
-/*   Updated: 2023/01/11 18:08:50 by handler          ###   ########.fr       */
+/*   Created: 2022/04/25 13:47:42 by mcombeau          #+#    #+#             */
+/*   Updated: 2022/04/30 17:39:25 by mcombeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-// # include <stdio.h>
-# include <unistd.h>
+# include <stddef.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdio.h>
 
-typedef struct s_pile
+typedef struct s_stack
 {
 	int				value;
-	struct s_pile	*prev;
-	struct s_pile	*next;
-}					t_pile;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
 
-typedef struct s_mq
-{
-	int	med;
-	int	q1;
-	int	q2;
-	int	len;
-}		t_mq;
+/* Initialization */
 
-char		**ft_split(char const *s, char c);
-size_t		ft_strlen(const char *s);
-long long	ft_atoi(const char *nptr);
-int			ft_atoi_and_check(const char *nptr, int *error);
-char		*ft_strdup(const char *src);
-void		quick_sort(int array[], int low, int high);
-void		get_median_quartil(char **spl, t_mq **mq, int *error);
-void		fill_mq(t_mq **mq, int len, int array[]);
-void		free_everything(t_pile **pile_a, t_pile **pile_b, t_mq **mq);
+t_stack		*fill_stack_values(int ac, char **av);
+void		assign_index(t_stack *stack_a, int ac);
 
-void		print_list(t_pile *head);
+/* Sorting Algorithms */
 
-// list_utils
-int			new_element(int nbr, t_pile **elem);
-void		modify_element(t_pile **elem, t_pile *prev, t_pile *next);
-int			add_to_list(t_pile **pile, int nbr);
-void		free_pile(t_pile **pile);
-int			len(t_pile *pile);
+int			is_sorted(t_stack *stack);
+void		tiny_sort(t_stack **stack);
+void		sort(t_stack **stack_a, t_stack **stack_b);
 
-// getter
-int			get_index(t_pile *pile, int elem);
-int			get_min(t_pile *pile);
-int			get_max(t_pile *pile);
-int			get_pos_to_place(t_pile *pile, int elem);
-int			get_best_move(t_pile *pile_a, t_pile *pile_b);
+/* Position */
 
-// swap
-void		swap_a(t_pile **pile_a);
-void		swap_b(t_pile **pile_b);
+int			get_lowest_index_position(t_stack **stack);
+void		get_target_position(t_stack **stack_a, t_stack **stack_b);
 
-// push
-void		push_a(t_pile **pile_a, t_pile **pile_b, t_mq **mq);
-void		push_b(t_pile **pile_a, t_pile **pile_b, t_mq **mq);
+/* Cost */
 
-// rotate
-void		rotate_both(t_pile **p_a, t_pile **p_b);
-void		rotate_a(t_pile **pile_a, int print);
-void		rotate_b(t_pile **pile_b, int print);
+void		get_cost(t_stack **stack_a, t_stack **stack_b);
+void		do_cheapest_move(t_stack **stack_a, t_stack **stack_b);
 
-void		reverse_rotate_both(t_pile **p_a, t_pile **p_b);
-void		reverse_rotate_a(t_pile **pile_a, int print);
-void		reverse_rotate_b(t_pile **pile_b, int print);
+/* Calculate Move */
 
-//algo
-void		sort_three(t_pile **pile_a);
-int			is_sorted(t_pile *pile);
-int			inner(t_pile *pile, int q1, int q2);
-void		replace_pile(t_pile **pile);
-void		insertion_sort(t_pile **p_a, t_pile **p_b, int b_move, t_mq **mq);
-void		push_b_optimize(t_pile *pile_a, t_pile *pile_b, t_mq *mq);
-void		push_a_optimize(t_pile *pile_a, t_pile *pile_b, t_mq *mq);
+void		do_move(t_stack **a, t_stack **b, int cost_a, int cost_b);
+
+/* Operations */
+
+void		do_pa(t_stack **stack_a, t_stack **stack_b);
+void		do_pb(t_stack **stack_a, t_stack **stack_b);
+void		do_sa(t_stack **stack_a);
+void		do_sb(t_stack **stack_b);
+void		do_ss(t_stack **stack_a, t_stack **stack_b);
+void		do_ra(t_stack **stack_a);
+void		do_rb(t_stack **stack_b);
+void		do_rr(t_stack **stack_a, t_stack **stack_b);
+void		do_rrr(t_stack **stack_a, t_stack **stack_b);
+void		do_rra(t_stack **stack_a);
+void		do_rrb(t_stack **stack_b);
+
+/* Stack Functions */
+
+t_stack		*get_stack_bottom(t_stack *stack);
+t_stack		*get_stack_before_bottom(t_stack *stack);
+t_stack		*stack_new(int value);
+void		stack_add_bottom(t_stack **stack, t_stack *new);
+int			get_stack_size(t_stack	*stack);
+
+/* Utils */
+
+void		free_stack(t_stack **stack);
+long int	ft_atoi(const char *str);
+void		ft_putstr(char *str);
+int			nb_abs(int nb);
+
+/* Error */
+
+void		exit_error(t_stack **stack_a, t_stack **stack_b);
+
+/* Input Check */
+
+int			is_correct_input(char **av);
+int			is_digit(char c);
+int			is_sign(char c);
+int			nbstr_cmp(const char *s1, const char *s2);
 
 #endif
